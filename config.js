2f48c9e12db99bd008ffa42340659f5e0b28a6b3
turbogaming <101031058+turbogaming876@users.noreply.github.com>
2022-05-06 13:51:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.owner = ['917373104400','917560983211'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['916909137213'917560983211'] //another owner number
+global.premium = ['916909137213'917560983211'] //premium number
+global.pengguna = 'Darco AND Vampire' //username
+global.botnma = 'DARCO VAMPIRE Bot-MD' //bot name
+global.ownernma = 'DARCO AND VAMPIRE' //owner name
+global.packname = 'DARCO VAMPIRE Bot' //sticker package name
+global.author = 'By Darco And Vampire' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
